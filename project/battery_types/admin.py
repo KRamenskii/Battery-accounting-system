14951a67db29c8eaed3ee8f6fd10441f8fe586e3
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-01-10 20:12:30 +0700

    Added admin settings for the BatteryType model: display, search and filtering.

--- project/battery_types/admin.py
+++ project/battery_types/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BatteryType
 
-# Register your models here.
+@admin.register(BatteryType)
+class BatteryTypeAdmin(admin.ModelAdmin):
+    list_display = ('manufacturer', 'battery_type_title', 'nominal_voltage', 'nominal_capacity_20')
+    search_fields = ('manufacturer', 'battery_type_title')
+    list_filter = ('manufacturer',)
--- project/battery_types/models.py
+++ project/battery_types/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class BatteryType(models.Model):
+    manufacturer = models.CharField(
+        verbose_name = 'Производитель',
+        max_length=255
+    )
+    battery_type_title = models.CharField(
+        verbose_name = 'Модель',
+        max_length=255
+    )
+    nominal_voltage = models.DecimalField(
+        verbose_name = 'Номинальное напряжение, В',
+        max_digits=10,
+        decimal_places=2
+    )
+    elements_count = models.IntegerField(verbose_name = 'Количество элементов в АКБ')
+    lifespan = models.IntegerField(verbose_name = 'Скрок службы, лет')
+    nominal_capacity_20 = models.DecimalField(
+        verbose_name = 'Номинальная ёмкость при 20-ти часовом разряде',
+        max_digits=10, 
+        decimal_places=2
+    )
+    nominal_capacity_10 = models.DecimalField(
+        verbose_name = 'Номинальная ёмкость при 10-ти часовом разряде',
+        max_digits=10, 
+        decimal_places=2
+    )
+    nominal_capacity_5 = models.DecimalField(
+        verbose_name = 'Номинальная ёмкость при 5-ти часовом разряде',
+        max_digits=10, 
+        decimal_places=2
+    )
+    self_discharge = models.DecimalField(
+        verbose_name = 'Саморазряд, %',
+        max_digits=10, 
+        decimal_places=2
+    )
+    internal_resistance = models.DecimalField(
+        verbose_name = 'Внутреннее сопротивление, мОм',
+        max_digits=10, 
+        decimal_places=2
+    )
+
+    class Meta:
+        verbose_name = 'Тип АКБ'
+        verbose_name_plural = 'Типы АКБ'
+
+    def __str__(self):
+        return self.battery_type_title
