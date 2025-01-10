5db038870ceb6a25d1f9ffccd482a00e65dfafbc
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-01-10 16:07:06 +0700

    Added models of organizations, services, areas and positions; fixed migrations.

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Role, CustomUser, Organization, Department, SubDepartment, Job
 
-# Register your models here.
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'role_title')
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('role', 'is_staff', 'is_active')
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'organization_title')
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'department_title', 'organization')
+    list_filter = ('organization',)
+
+@admin.register(SubDepartment)
+class SubDepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subdepartment_title', 'department')
+    list_filter = ('department',)
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'job_title')
--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# Модель роли
+class Role(models.Model):
+    role_title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.role_title
+
+# Модель организации
+class Organization(models.Model):
+    organization_title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.organization_title
+
+# Модель службы
+class Department(models.Model):
+    department_title = models.CharField(max_length=255)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.department_title
+
+# Модель участка
+class SubDepartment(models.Model):
+    subdepartment_title = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subdepartment_title
+
+# Модель должности
+class Job(models.Model):
+    job_title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.job_title
+
+# Модель Пользователя
+class CustomUser(AbstractUser):
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+    user_name = models.CharField(max_length=20)
+    sur_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    first_phone_number = models.CharField(max_length=14)
+    second_phone_number = models.CharField(max_length=4, blank=True, null=True)
+
+    # Связь с организацией, службой и участком
+    organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    subdepartment = models.ForeignKey(SubDepartment, on_delete=models.SET_NULL, null=True)
+    job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.sur_name} {self.user_name}"
