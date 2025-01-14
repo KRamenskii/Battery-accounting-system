340377d145fb1477ea3c0b12139e7acbcd053511
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-01-14 18:30:15 +0700

    Display of battery types has been configured.

--- project/battery_types/urls.py
+++ project/battery_types/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('battery_types', views.battery_types, name='battery_types')
+    path('battery_types', views.battery_types_view, name='battery_types')
 ]
--- project/battery_types/views.py
+++ project/battery_types/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import BatteryType
 
-def battery_types(request):
-    return render(request, 'battery_types/battery_types.html')
+def battery_types_view(request):
+    battery_types = BatteryType.objects.all()
+    return render(request, 'battery_types/battery_types.html', {'battery_types': battery_types})
