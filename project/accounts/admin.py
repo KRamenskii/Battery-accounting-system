5b803a2a5c5a495ba4bbcc38cd1cb663026fc1a8
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-11-28 15:13:31 +0700

    Fix: model and password in the admin panel

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -1,15 +1,71 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import Role, CustomUser, Organization, Department, SubDepartment, Job, ErrorType, ErrorStatus, ErrorReport
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = (
+            'username', 'email', 'sur_name', 'user_name', 'last_name',
+            'first_phone_number', 'second_phone_number',
+            'organization', 'department', 'subdepartment', 'job', 'role'
+        )
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = '__all__'
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ('username', 'email', 'role', 'is_staff', 'sur_name', 'user_name')
+    search_fields = ('username', 'email', 'sur_name', 'user_name')
+    list_filter = ('role', 'is_staff', 'is_active')
+
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('username', 'password')
+        }),
+        ('Персональные данные', {
+            'fields': (
+                'sur_name', 'user_name', 'last_name', 'email',
+                'first_phone_number', 'second_phone_number'
+            )
+        }),
+        ('Организация', {
+            'fields': ('organization', 'department', 'subdepartment', 'job', 'role')
+        }),
+        ('Права доступа', {
+            'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ('Даты', {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+
+    add_fieldsets = (
+        ('Создание пользователя', {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'password1', 'password2',
+                'sur_name', 'user_name', 'last_name', 'email',
+                'first_phone_number', 'second_phone_number',
+                'organization', 'department', 'subdepartment', 'job', 'role',
+                'is_staff', 'is_active'
+            )
+        }),
+    )
+
+    ordering = ('username',)
 
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
     list_display = ('id', 'role_title')
-
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'role', 'is_staff', 'first_name', 'last_name')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    list_filter = ('role', 'is_staff', 'is_active')
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
