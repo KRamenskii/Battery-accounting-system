6b11d950f401e76576a329821bfb2ca3b86bd733
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-12-19 15:45:36 +0700

    Add: events page

--- project/events/views.py
+++ project/events/views.py
@@ -1,4 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Event
 
 def events_list(request):
-    return render(request, "events/events_list.html")
+    events = Event.objects.all()
+
+    paginator = Paginator(events, 20)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "events/events_list.html", {
+        "page_obj": page_obj
+    })
