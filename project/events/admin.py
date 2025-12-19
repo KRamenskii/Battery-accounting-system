460ccd7db417989bedc115c90d3d2fbd71744eed
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2025-12-19 12:45:23 +0700

    Add: events filter on admin page

--- project/events/admin.py
+++ project/events/admin.py
@@ -6,3 +6,4 @@ from .models import Event
 class EventAdmin(admin.ModelAdmin):
     list_display = ("event_type", "user", "content_type", "object_id", "timestamp")
     readonly_fields = ("user", "event_type", "timestamp", "content_type", "object_id", "object_repr", "old_data", "new_data", "changed_fields")
+    list_filter = ('event_type', 'user')
